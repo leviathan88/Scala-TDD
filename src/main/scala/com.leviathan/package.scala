2149a2dff5da53ef94f716f7ecf6acc2fc680de4
leviathan88 <levigithuber@gmail.com>
2016-11-05 13:33:51 +0100

    adding a require to our Number trait

--- src/main/scala/com.leviathan/package.scala
+++ src/main/scala/com.leviathan/package.scala
@@ -2,6 +2,7 @@ package com
 package object leviathan {
   trait Number {
     def number:String
+    require(number forall Character.isDigit, "Unable to convert string to number")
   }
   case class Decimal(number:String) extends Number
   case class Binary(number:String) extends Number
